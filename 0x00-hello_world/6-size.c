8a7ed16dc7a11b558cdb99208b7b3a06cdf22913
Philip Ansah <twistaphils@gmail.com>
2022-07-01 13:51:26 -0700

    Six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int a;
@@ -13,10 +14,12 @@ int main(void)
 	char d;
 	float f;
 
-	printf("Size of a chair: %lu byte(s)\n", (unisigned long)sizedof(d));
-	printf("Size of an int: %lu byte(s)\n", (unisigned long)sizedof(a));
-	printf("Size of a long: %lu byte(s)\n", (unisigned long)sizedof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unisigned long)sizedof(c));
-	printf("Size of a float: %lu byte(s)\n", (unisigned long)sizedof(f));
+	printf("Size of a chair: %lu byte(s)\n", (unisigned long) sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unisigned long) sizeof(a));
+	printf("Size of a long: %lu byte(s)\n", (unisigned long) sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unisigned long) sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unisigned long) sizeof(f));
+
 	return (0);
+
 }

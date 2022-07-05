cdba22ba53afdbdb9d78071b7bb7ca5bb9731391
Philip Ansah <twistaphils@gmail.com>
2022-07-05 15:51:10 -0700

    Task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - Write a program that prints _putchar
- *
- * Return: Always 0 (success)
+ * main - main block
+ * Description: print _putchar followed on a new line
+ * Return: always 0
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
